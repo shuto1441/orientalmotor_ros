d9c337aaee5b7877a36b53936b28969fcb20a4bd
shuto1441 <shuto1441@gmail.com>
2021-11-16 18:21:03 +0900

    gui modified

--- scripts/modbus_motor_reconfigure.py
+++ scripts/modbus_motor_reconfigure.py
@@ -19,28 +19,55 @@ class modbus_ros():
         print(self.client.name)
         self.srv = Server(modbus_motorConfig, self.callback)
         self.master = tk.Tk()
-        button = tk.Button(self.master, text="rotate", command=lambda: self.motor_rotate(motor))
-        button.pack()
+        button1 = tk.Button(self.master, text="positiong rotate", command=lambda: self.positioning_rotate(motor))
+        button1.pack()
+        button2 = tk.Button(self.master, text="return to origin", command=lambda:self.return_to_origin())
+        button2.pack()
+        button3 = tk.Button(self.master, text="continuous rotate", command=lambda: self.continuous_rotate(motor))
+        button3.pack()
+        button4 = tk.Button(self.master, text="stop rotate", command=lambda:self.off())
+        button4.pack()
         self.master.mainloop()
 
     def callback(self, config, level):
-        rospy.loginfo("""Reconfigure Request: {rpm}, {angle}""".format(**config))
+        rospy.loginfo("""Reconfigure Request: {rpm}, {angle}, {reverse}""".format(**config))
         motor.rpm = int("{rpm}".format(**config))
         motor.angle = int("{angle}".format(**config))
+        motor.reverse = "{reverse}".format(**config)
         return config
 
-    def motor_rotate(self,msg):
+    def positioning_rotate(self,msg):
         rpm = msg.rpm
-        angle = msg.angle
+        acceleration = msg.acceleration
+        deceleration = msg.deceleration
         self.apply_operation_method()
         time.sleep(0.1)
-        self.apply_angle(angle)
+        self.apply_angle(msg)
         time.sleep(0.1)
         self.apply_rpm(rpm)
         time.sleep(0.1)
-        self.start()
+        # self.apply_acceleration(acceleration)
+        # time.sleep(0.1)
+        # self.apply_deceleration(deceleration)
+        # time.sleep(0.1)
+        self.start_on()
         time.sleep(0.1)
-        self.stop()
+        self.off()
+
+    def return_to_origin(self):
+        self.home_on()
+        time.sleep(1)
+        self.off()
+
+    def continuous_rotate(self,msg):
+        rpm = msg.rpm
+        reverse = msg.reverse
+        self.apply_rpm(rpm)
+        time.sleep(0.5)
+        if(reverse=="True"):
+            self.rvs_on()
+        else:
+            self.fwd_on()
 
     #運転データNo.0の運転方式
     def apply_operation_method(self):
@@ -61,18 +88,52 @@ class modbus_ros():
         # 結果は(上位→下位)の順
         return crc_register.to_bytes(2, 'little')
 
+    def angle_to_bytes(self,angle):
+        step = int(angle/self.pulse_angle)
+        command = step.to_bytes(4,byteorder='big')
+        return command
+
     def rpm_to_bytes(self,rpm):
         hz = int(rpm/60*360/self.pulse_angle)
         command = hz.to_bytes(2,byteorder='big')
         return command
+    
+    def rpm_acceleration_to_bytes(self,acceleration):
+        hz = int(acceleration/60*360/self.pulse_angle)
+        time = 1000/hz*1000
+        command = time.to_bytes(4,byteorder='big')
+        return command
+    
+    def apply_acceleration(self,msg):
+        acceleration = msg.acceleration
+        command = b"\x01\x06\x06\x01" + self.rpm_acceleration_to_bytes(acceleration)
+        command += self.error_check(command)
+        self.client.write(command)
+        result = self.client.read(self.size)
+        print("acceleration set: {}".format(result))
 
-    def angle_to_bytes(self,angle):
+    def apply_deceleration(self,msg):
+        deceleration = msg.deceleration
+        command = b"\x01\x06\x06\x81" + self.rpm_acceleration_to_bytes(deceleration)
+        command += self.error_check(command)
+        self.client.write(command)
+        result = self.client.read(self.size)
+        print("deceleration set: {}".format(result))
+
+    def angle_to_bytes_rvs(self,angle):
         step = int(-angle/self.pulse_angle)
         command = step.to_bytes(4,byteorder='big', signed=True)
         return command
 
-    def apply_angle(self,angle):
-        command = b"\x01\x10\x04\x00\x00\x02\x04" + self.angle_to_bytes(angle)
+    def apply_angle(self,msg):
+        angle = msg.angle
+        reverse = msg.reverse
+        print(msg.reverse)
+        print(reverse)
+        if(reverse=="True"):
+            command = b"\x01\x10\x04\x00\x00\x02\x04" + self.angle_to_bytes_rvs(angle)
+        else:
+            command = b"\x01\x10\x04\x00\x00\x02\x04" + self.angle_to_bytes(angle)
         command += self.error_check(command)
         self.client.write(command)
         result = self.client.read(self.size)
@@ -86,18 +147,42 @@ class modbus_ros():
         print("rpm set: {}".format(result))
 
     #START入力 ON （運転No.0 運転開始）
-    def start(self):
-        command = b"\x01\x06\x00\x7d\x00\x08\x18\x14"
+    def start_on(self):
+        command = b"\x01\x06\x00\x7d\x00\x08"
+        command += self.error_check(command)
         self.client.write(command)
         result = self.client.read(self.size)
         print("start on: {}".format(result))
 
     #START入力 OFF （運転No.0 運転終了）
-    def stop(self):
-        command = b"\x01\x06\x00\x7d\x00\x00\x19\xd2"
+    def off(self):
+        command = b"\x01\x06\x00\x7d\x00\x00"
+        command += self.error_check(command)
         self.client.write(command)
         result = self.client.read(self.size)
-        print("start off: {}".format(result))
+        print("off: {}".format(result))
+
+    def fwd_on(self):
+        command = b"\x01\x06\x00\x7d\x40\x00"
+        command += self.error_check(command)
+        self.client.write(command)
+        result = self.client.read(self.size)
+        print("fwd on: {}".format(result))
+
+    def rvs_on(self):
+        command = b"\x01\x06\x00\x7d\x80\x00"
+        command += self.error_check(command)
+        self.client.write(command)
+        result = self.client.read(self.size)
+        print("rvs on: {}".format(result))
+
+    def home_on(self):
+        command = b"\x01\x06\x00\x7d\x00\x10"
+        command += self.error_check(command)
+        self.client.write(command)
+        result = self.client.read(self.size)
+        print("home on: {}".format(result))
+
 
 
 if __name__ == "__main__":
