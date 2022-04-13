c6e2fc2aebe96477e96bf81dd7db389d04e23a23
shuto1441 <shuto1441@gmail.com>
2022-04-13 17:32:41 +0900

    oriental_motor_ros fixed

--- orientalmotor_ros/nodes/serial_node.py
+++ orientalmotor_ros/nodes/serial_node.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 
 import rospy
-from orientalmotor_ros import MotorReconfigure
+from orientalmotor_ros import OrientalMotor
+
 
 def main():
     rospy.init_node("motor_control")
-    sub = MotorReconfigure.OrientalMotor()
-
+    port = rospy.get_param('~port', '/dev/ttyUSB0')
+    sub = OrientalMotor(port)
     rospy.spin()
+
 
 if __name__ == '__main__':
     main()
---
+++ orientalmotor_ros/src/orientalmotor_ros/__init__.py
@@ -0,0 +1,1 @@
+from .MotorReconfigure import *
